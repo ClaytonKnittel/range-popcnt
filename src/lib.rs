f9285da8e2a069d8927a140cb1fe2cfb6d91a21f
claytonknittel <cknittel@wustl.edu>
2023-10-15 16:39:00 -0700

    range min

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,26 @@
 /// Returns the minumum/maximum value of x.count_ones() for x in range_min..range_max.
 
-pub fn get_max(range_min: u8, range_max: u8) -> u32 {
+pub fn range_popcnt_max(range_min: u8, range_max: u8) -> u32 {
+  debug_assert!(range_min < range_max);
   let different_bits = range_min ^ range_max;
   let highest_different_bit = 7 - different_bits.leading_zeros();
   let y = (1u8 << highest_different_bit) - 1;
   return (y | range_min).count_ones();
 }
 
-pub fn get_min(range_min: u8, range_max: u8) -> u32 {
+pub fn range_popcnt_min(range_min: u8, range_max: u8) -> u32 {
+  debug_assert!(range_min < range_max);
   if range_min == 0 {
     return 0;
   }
-  let different_bits = (range_min - 1) ^ (range_max - 1);
-  let highest_different_bit = 7 - different_bits.leading_zeros();
-  let p2 = 1u8 << highest_different_bit;
-  let mask = !(p2 - 1);
-  return ((range_min & mask) | p2).count_ones();
+  return 8 - range_popcnt_max(range_max.wrapping_neg(), range_min.wrapping_neg());
 }
 
 pub fn range_popcnt(range_min: u8, range_max: u8) -> (u32, u32) {
-  return (get_min(range_min, range_max), get_max(range_min, range_max));
+  return (
+    range_popcnt_min(range_min, range_max),
+    range_popcnt_max(range_min, range_max),
+  );
 }
 
 #[cfg(test)]
