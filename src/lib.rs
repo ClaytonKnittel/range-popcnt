d017472c48cf8602c0ce65fded000736fcd0bb45
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2023-10-15 16:42:38 -0700

    Minimum support (#2)
    * Add minimum support
    
    * range min
    
    * update docstring
    
    ---------
    
    Co-authored-by: Noah Goldstein <goldstein.w.n@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,49 @@
 /// Returns the maximum value of x.count_ones() for x in range_min..range_max.
-pub fn range_popcnt(range_min: u64, range_max: u64) -> u32 {
+pub fn range_popcnt_max(range_min: u8, range_max: u8) -> u32 {
   debug_assert!(range_min < range_max);
   let different_bits = range_min ^ range_max;
-  let highest_different_bit = 63 - different_bits.leading_zeros();
-  let y = (1u64 << highest_different_bit) - 1;
-  (y | range_min).count_ones()
+  let highest_different_bit = 7 - different_bits.leading_zeros();
+  let y = (1u8 << highest_different_bit) - 1;
+  return (y | range_min).count_ones();
+}
+
+/// Returns the minimum value of x.count_ones() for x in range_min..range_max.
+pub fn range_popcnt_min(range_min: u8, range_max: u8) -> u32 {
+  debug_assert!(range_min < range_max);
+  if range_min == 0 {
+    return 0;
+  }
+  return 8 - range_popcnt_max(range_max.wrapping_neg(), range_min.wrapping_neg());
+}
+
+/// Returns the minimum/maximum value of x.count_ones() for x in range_min..range_max.
+pub fn range_popcnt(range_min: u8, range_max: u8) -> (u32, u32) {
+  return (
+    range_popcnt_min(range_min, range_max),
+    range_popcnt_max(range_min, range_max),
+  );
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
-  fn range_popcnt_naive(range_min: u64, range_max: u64) -> u32 {
-    (range_min..range_max)
-      .map(|x| x.count_ones())
-      .max()
-      .unwrap()
+  fn range_popcnt_naive(range_min: u8, range_max: u8) -> (u32, u32) {
+    return (
+      (range_min..range_max)
+        .map(|x| x.count_ones())
+        .min()
+        .unwrap(),
+      (range_min..range_max)
+        .map(|x| x.count_ones())
+        .max()
+        .unwrap(),
+    );
   }
 
   #[test]
   fn test_range_popcnt() {
-    for range_max in 1..512 {
+    for range_max in 1..=255 {
       for range_min in 0..range_max {
         assert_eq!(
           range_popcnt(range_min, range_max),
